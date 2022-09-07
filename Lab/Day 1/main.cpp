2576ba3fc6b13c25f407907ce91bce8dd8a6645c
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-07 15:06:14 +0530

    Update main.cpp

--- Lab/Day 1/main.cpp
+++ Lab/Day 1/main.cpp
@@ -1,36 +1,103 @@
 /******************************************************************************
 ADT- rational number
 
+value defination
+integer numerator
+integer denominator
+
+
+preconditions-
+denominator must not be 0
+
+Operator defination.
+abstract rationaltype add(rationaltype a, rationaltype b){
+preconditions none
+
+preconditions - 
+function returs a new rationaltype.
+new numerator value is (a.numerator)*(b.denominator)+(b.numerator)*(a.denominator)
+new denominator (a.denominator)*(b.denominator)
+
+    
+}
+
+
+
+abstract rationaltype sub(rationaltype a, rationaltype b){
+preconditions none
+
+preconditions - 
+function returs a new rationaltype.
+new numerator value is (a.numerator)*(b.denominator)-(b.numerator)*(a.denominator)
+new denominator (a.denominator)*(b.denominator)
+    
+}
+
+
+
+
+abstract rationaltype multiply(rationaltype a, rationaltype b){
+preconditions none
+
+preconditions - 
+function returs a new rationaltype.
+new numerator value is (a.numerator)*(b.numerator)
+new denominator (a.denominator)*(b.denominator)
+    
+}
+
+abstract rationaltype divide(rationaltype a, rationaltype b){
+preconditions none
+
+preconditions - 
+function returs a new rationaltype.
+new numerator value is (a.numerator)*(b.denominator)
+new denominator (a.denominator)*(b.numerator)
+    
+}
+
+
+
+abstract boolean equate(rationaltype a, rationaltype b){
+preconditions none
+
+preconditions - 
+returns true iff a.numerator==b.numerator AND a.denominator==b.denominator 
+else false
+}
+
 *******************************************************************************/
 #include <iostream>
 
 using namespace std;
- class rational{
+ 
+
+class rational{
      public:
-    int p;
-    int q;
+    int numerator;
+    int denominator;
     public: rational(int p, int q){
      if (q==0){
          throw std::invalid_argument( "received infinity value" );
      }
-        this->p=p;
-       this->q=q;
+        this->numerator=p;
+       this->denominator=q;
     }
     static rational* add(rational a, rational b){
-     return new rational((a.p)*(b.q)+(b.p)*(a.q),(a.q)*(b.q));
+     return new rational((a.numerator)*(b.denominator)+(b.numerator)*(a.denominator),(a.denominator)*(b.denominator));
     }
      static rational* sub(rational a, rational b){
-     return new rational((a.p)*(b.q)-(b.p)*(a.q),(a.q)*(b.q));
+     return new rational((a.numerator)*(b.denominator)-(b.numerator)*(a.denominator),(a.denominator)*(b.denominator));
     }
      static rational* multiply(rational a, rational b){
-     return new rational((a.p)*(b.p),(a.q)*(b.q));
+     return new rational((a.numerator)*(b.numerator),(a.denominator)*(b.denominator));
     }
     
     static rational* divide(rational a, rational b){
-     return new rational((a.p)*(b.q),(a.q)*(b.p));
+     return new rational((a.numerator)*(b.denominator),(a.denominator)*(b.numerator));
     }
     static bool equate(rational a, rational b){
-        if(a.p==b.p&a.q==b.q){
+        if(a.numerator==b.numerator&a.denominator==b.denominator){
             return true;
         }
         else{
@@ -44,7 +111,7 @@ int main()
     rational *a = new rational(2,3);
     rational *b = new rational(3,4);
     rational *c = rational::divide(*a,*b);
-    cout<<c->p<<"\n";
+    cout<<c->numerator<<"\n";
     cout<<rational::equate(*a,*b);
     return 0;
 }
