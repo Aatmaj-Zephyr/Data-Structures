56e5a32b1213d83e77c845224ee3da0a0af4c9ef
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-15 15:12:06 +0530

    Update main.cpp

--- Lab/Day 2/main.cpp
+++ Lab/Day 2/main.cpp
@@ -1,9 +1,5 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+Stack- dynamic implementation.
 
 *******************************************************************************/
 #include <iostream>
@@ -37,12 +33,7 @@ class stack_element{
     void point(stack_element* next_element){
     next=next_element;
     }
-    void print(){
-        cout<<"\n"<<value;
-        /*if(next!=NULL){
-        next->print();
-        }*/
-    }
+   
     
 };
 
@@ -52,41 +43,36 @@ class stack_element{
     
     stack_element top;
    stack(){
-      // top = new stack_element();
-       //null = new stack_element();
+      
        top.point(&null);
-       null.set_value(0);
-      // null.point(NULL)
-        top.set_value(100);
+      
    }
 void push(int value){
-   // print();
   stack_element *s = new stack_element();
-  cout<<"\n"<<s->value;
 
  s->set_value(value);
  s->point(top.get_next());
+ cout<<"\n"<<s->value<<" is pushed.";
  top.point(s);
 
 }
 int pop(){
     int temp =top.get_next()->get_value();
     top.point(top.get_next()->get_next());
-    cout<<"\n"<<temp;
+    cout<<"\n"<<temp<<" is popped.";
     return temp;
 }
 void print(){
-     stack_element p;
-     p.point(&top);
-   //  while(&*p.get_next()!=&null){
-         for (int i=5;i>=0;i--){
-    int temp2 =p.get_next()->get_value();
+     cout<<"\n"<<"stack contents are-- ";
+     stack_element *p = new stack_element();
+     p->point(top.get_next());
+     while(&*p->get_next()!=&null){
+    int temp2 =p->get_next()->get_value();
     cout<<"\n"<<temp2;
-         p.point(p.get_next()->get_next());
+         p->point(p->get_next()->get_next());
      }
-     delete &p;
-    //top.get_next().print();
-   // top.print();
+     delete p;
+    
 }
 };
 
@@ -94,20 +80,18 @@ int main()
 {
     cout<<"Hello World";
     stack my_stack;
-   // my_stack.push(2);
+    my_stack.push(2);
     my_stack.push(3);
     
     my_stack.push(5);
-   my_stack.pop();
-   my_stack.pop();
+    my_stack.pop();
+    my_stack.pop();
+    my_stack.push(7);
+    my_stack.push(5);
 
     my_stack.print();
     
-    /*for(int i=10;i<=0;i--){
-        stack_element temp;
-        temp
-    }
-    */
+    
    
     return 0;
 }
