45c5d9b88e306700c783bae28bf3d3aaf8663e9e
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-10-02 19:25:53 +0530

    Update polynomial.cpp

--- Lab/Day 5/polynomial.cpp
+++ Lab/Day 5/polynomial.cpp
@@ -1,9 +1,5 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+Add two polynomials
 
 *******************************************************************************/
 #include <iostream>
@@ -54,7 +50,7 @@ class linkedlist{
          temp=&*bottom;
 
         while(temp->pointer!=NULL){
-            cout<<temp->pointer->value;
+            cout<<"\n"<<temp->pointer->value<<"*"<<"x^("<<temp->pointer->priority<<")";
             temp->point(temp->pointer->pointer);
         }
     }
@@ -63,41 +59,57 @@ class linkedlist{
 
 linkedlist* addPoly(linkedlist a,linkedlist b){
  linkedlist *c = new linkedlist();
- node *temp1=a.bottom->pointer;
- node *temp2=b.bottom->pointer;
- while(true){
- if(temp1->value == temp2->value){
-     c->add(temp1->value+temp2->value,temp1->priority);
-      temp1->point(temp1->pointer->pointer);
-      temp2->point(temp2->pointer->pointer);
+ node *temp1;
+ temp1->point(a.bottom->pointer);
+ node *temp2;
+ temp2->point(b.bottom->pointer);
+ 
+ 
+ 
+ for (int i=0;0==0;i++){
+ //Only for integral powers of x
+ //assuming sorted polynomials
+ int temp3=0;
+  if(temp1->pointer==NULL && temp2->pointer==NULL)
+ {
+     break;
  }
- if(temp1->value > temp2->value){
-     c->add(temp1->value,temp1->priority);
-      temp1->point(temp1->pointer->pointer);
+ if(temp1->pointer!=NULL && temp1->pointer->priority==i){
+ temp3=temp3+temp1->pointer->value;
+ temp1->point(temp1->pointer->pointer);
  }
- if(temp1->value < temp2->value){
-     c->add(temp2->value,temp2->priority);
-       temp2->point(temp2->pointer->pointer);
+ 
+ if(temp2->pointer!=NULL && temp2->pointer->priority==i){
+ temp3=temp3+temp2->pointer->value;
+ temp2->point(temp2->pointer->pointer);
+ //cout<<temp3;
+ }
+ c->add(temp3,i);
 
  }
- if(temp1->pointer == NULL){
-              cout<<"b";
-
-     return c;
- }
-if(temp2->pointer == NULL){
-              cout<<"a";
-
-     return c;
-
- }
-
- }
+free(temp2);
+free(temp1);
+free(c);
  return c;
 };
+
+linkedlist* minusPoly(linkedlist a){
+    node* temp=a.bottom;
+ linkedlist *c = new linkedlist();
+    while(temp->pointer!=NULL){
+        c->add(-temp->pointer->value,temp->pointer->priority);
+        temp->point(temp->pointer->pointer);
+    }
+    return c;
+}
+
+linkedlist* subPoly(linkedlist a,linkedlist b){
+    
+    return addPoly(a,*minusPoly(b));
+}
 int main()
 {
-    cout<<"Hello World";
+  
 linkedlist *mylink=new linkedlist();
 mylink->add(2,1);
 mylink->add(20,2);
@@ -105,12 +117,15 @@ mylink->add(14,4);
 //mylink->display();
 
 linkedlist *mylink2=new linkedlist();
+mylink2->add(7,0);
 mylink2->add(2,1);
-mylink2->add(30,3);
+mylink2->add(31,3);
 mylink2->add(5,4);
-
+cout<<"Hello";
 //mylink2->display();
 linkedlist *c = addPoly(*mylink,*mylink2);
-c->display();
+//c->display();
+linkedlist *d = subPoly(*mylink,*mylink2);
+d->display();
     return 0;
 }
