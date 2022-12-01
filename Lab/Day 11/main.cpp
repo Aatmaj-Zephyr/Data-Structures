ca91afda734b39bdc077b38e8b78d114926c05b0
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-12-01 10:33:57 +0530

    Update main.cpp

--- Lab/Day 11/main.cpp
+++ Lab/Day 11/main.cpp
@@ -4,12 +4,15 @@
 
 using namespace std;
 
+//for graph of fixed size 6
+ine len=6;
 int ctr=0;
-int visited[6]={0,0,0,0,0};
-    int adjustancy[6][6]={{0,1,1,1,1},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0}} ;
-bool elementnotinvisited(int j){
+int visited[6]={-1,-1,-1,-1,-1,-1};
+int adjustancy[6][6];//{{0,1,1,1,1},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0}} ;
+bool searchinvisited(int j){
     for(int i=0;i<6;i++){
      if(visited[i]==j){
+        // cout<<j;
          return true;
      }   
     }
@@ -20,10 +23,12 @@ void visitedadd(int j){
     ctr++;
 }
 int bfs( int value){
+    visitedadd(value);
     for(int i=0;i<6;i++){
         if(adjustancy[value][i]==1){
-            if(!elementnotinvisited(i)){
-                visitedadd(i);
+          //  cout<<(searchinvisited(i));
+            if(!searchinvisited(i)){ //element not visited already
+                
                 bfs(i);
             }
         }
@@ -36,20 +41,16 @@ int main()
 {   cout<<"Hello World \n";
 
 int ans[6];
-/*
-    for(int i=1;i<6;i++){
-        int temp;
-        cin>>temp;
-        adjustancy[i][0]=temp;
-        adjustancy[0][i]=adjustancy[i][0];
-    }
-    cout<<adjustancy;
-    for(int i=1;i<6;i++){
-       for(int j=1;j<6;j++){
-       cout<<"Join "<<adjustancy[i][0]<<" and "<<adjustancy[0][j]<<"? \n";
+
+
+  
+    for(int i=0;i<6;i++){
+       for(int j=0;j<6;j++){
+       cout<<"Join "<<i<<" and "<<j<<"? \n";
        cin>>adjustancy[i][j];
     }
-    */
+    }
+    
     bfs(2);
         
     
